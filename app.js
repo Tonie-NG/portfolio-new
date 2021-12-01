d59e7ffd79594d33d4c6eec2773cd015231ee7c7
Tonie-NG <victornwanochi@gmail.com>
2021-12-02 00:54:58 +0100

    first commit to github

---
+++ app.js
@@ -0,0 +1,22 @@
+//Imports
+const express = require('express')
+const app = express()
+const port = 3000
+
+//static
+app.use(express.static('h'))
+app.use(express.static(__dirname + 'h'))
+app.use(express.static(__dirname + 'h'))
+app.use('/assets'+'/images', express.static(__dirname + 'h/assets/images'))
+
+app.get('', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+
+
+//Listen
+app.listen(port, () => console.info(`Listening at port ${port}`))
+
+
