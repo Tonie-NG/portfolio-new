47f36c3e6389334b539ec8759a31e2c60ab44d95
Tonie-NG <victornwanochi@gmail.com>
2021-12-03 02:11:21 +0100

    we back

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Imports
 const express = require('express')
 const app = express()
-const port = process.env.PORT|| 3000
+const port = process.env.PORT|| 5000
 
 //static
 app.use(express.static('h'))
